ab5b9f6891e179d305ed18d181557d31a7ae33c9
richiever <richie206080@gmail.com>
2022-10-21 10:43:11 +1100

    basicbuttons & operands

--- external.js
+++ external.js
@@ -14,7 +14,22 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(divide(1,2));
-console.log(multiply(1,2));
+function operate(oper, num1, num2) {
+    if(oper === '+') {
+        return add(num1,num2);
+    }
+    else if(oper === '-') {
+        return subtract(num1,num2);
+    }
+    else if(oper === '/') {
+        return divide(num1,num2);
+    }
+    else if(oper === '*') {
+        return multiply(num1,num2);
+    }
+    else {
+        return "This is an invalid input!";
+    }
+}
+
+
