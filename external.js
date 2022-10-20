201548b2464f72ed62a0eff9235472e2375a72c9
richiever <richie206080@gmail.com>
2022-10-21 10:26:30 +1100

    add, sub, div, mul funcs completed

---
+++ external.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(add(1,2));
+console.log(subtract(1,2));
+console.log(divide(1,2));
+console.log(multiply(1,2));
